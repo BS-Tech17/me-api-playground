c0f4ca8d3170a1a9cf8048a898af3a1e7cb9c7ba
BS-Tech17 <bs17302004@gmail.com>
2025-09-01 03:10:44 +0530

    Update schema.sql

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,1 +1,10 @@
- 
+ CREATE TABLE IF NOT EXISTS profile (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    education TEXT,
+    skills JSONB,
+    projects JSONB,
+    work JSONB,
+    links JSONB
+); 
