2cf395e6c80d5dfb66c8e9ee50031ab42eb0d598
BS-Tech17 <bs17302004@gmail.com>
2025-09-01 13:39:16 +0530

    Update seed.sql

--- backend/seed.sql
+++ backend/seed.sql
@@ -6,7 +6,7 @@ INSERT INTO profile (id, name, email, education, skills, projects, work, links) 
     '["Python", "Machine Learning", "IoT Solutions", "Web Development"]'::jsonb,
     '[{"title": "Project 1", "description": "Built a hand detection model using python", "links": ["https://github.com/BS-Tech17/NLP-for-document-summarizing"]}, {"title": "Project 2", "description": "AI-Based document summarization", "links": ["https://github.com/BS-Tech17/Hand-Detection-using-AI"]}]'::jsonb,
     '[{"title": "Intern at Company Clearleaff Solutions LLP Noida", "description": "Worked as a Web developer for making an IoT simulator software"}, {"title": "AI Intern", "description": "Developed an AI-based model for pseudocoloring of images in different terrains "}]'::jsonb,
-    '{"github": "https://github.com/BS-Tech17",  "portfolio": "https://portfolio-placeholder.com"}'::jsonb
+    '{"github": "https://github.com/BS-Tech17", "linkedin": "https://linkedin.com/in/yourname",  "portfolio": "https://portfolio-placeholder.com"}'::jsonb
 ) ON CONFLICT (id) DO UPDATE SET
     name = EXCLUDED.name,
     email = EXCLUDED.email,
