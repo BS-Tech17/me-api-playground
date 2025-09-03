e584e3cae6a1fb01493e175756b8ba1134417f42
BS-Tech17 <bs17302004@gmail.com>
2025-09-03 15:20:29 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,13 @@
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from backend.database import (
-    Profile,
-    get_profile,
-    create_or_update_profile,
-    delete_profile,
-    get_projects_by_skill,
-    get_top_skills,
-    search,
-)
+from backend.database import Profile, get_profile, create_or_update_profile, get_projects_by_skill, get_top_skills, search
 from typing import Optional
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "https://me-api-playground-w35g.onrender.com"],
+    allow_origins=["http://localhost:8000", "https://me-api-playground-i6jj.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -30,7 +21,7 @@ def health():
 def read_profile():
     profile = get_profile()
     if not profile:
-        raise HTTPException(status_code=404, detail="Profile not found")
+        return {"error": "Profile not found"}
     return profile
 
 @app.post("/profile", response_model=Profile)
@@ -40,14 +31,6 @@ def create_profile(profile: Profile):
 @app.put("/profile", response_model=Profile)
 def update_profile(profile: Profile):
     return create_or_update_profile(profile)
-
-@app.delete("/profile", response_model=dict)
-def remove_profile():
-    profile = get_profile()
-    if not profile:
-        raise HTTPException(status_code=404, detail="Profile not found")
-    delete_profile()
-    return {"message": "Profile deleted successfully"}
 
 @app.get("/projects")
 def projects_by_skill(skill: Optional[str] = Query(None)):
@@ -62,6 +45,4 @@ def top_skills():
 
 @app.get("/search")
 def search_query(q: str = Query(...)):
-    return search(q)
-
-app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
+    return search(q) 
